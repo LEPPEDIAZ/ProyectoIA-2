dc8d0156e24b61181cb855318ddeca56a83bad74
Unknown <dia151378@uvg.edu.gt>
2019-05-03 01:45:44 -0600

    sigmoid backward

--- functions/redesneuronales.py
+++ functions/redesneuronales.py
@@ -37,6 +37,13 @@ def sigmoid2(Z):
     A = 1 / (1 + np.exp(-Z))
     cache = Z
     return A, cache
+def sigmoid_backward(dA, cache):
+    Z = cache
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    assert (dZ.shape == Z.shape)
+    return dZ
+
 def tanh(Z):
     A = np.tanh(Z)
     return A, Z
