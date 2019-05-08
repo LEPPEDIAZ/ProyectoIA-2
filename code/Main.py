cb147c34f5016daf1e7a5158260a2426a26fb72e
Unknown <dia151378@uvg.edu.gt>
2019-05-08 17:19:56 -0600

    very happy is all done
    now is time to clean code for presentation

--- code/Main.py
+++ code/Main.py
@@ -1,8 +1,9 @@
-#import os
+import os
 print("Reading...")
-#os.system("python Proyecto2-parte1.py")
-#os.system("python Proyecto2-parte2.py")
-#os.system("python Proyecto2-parte3.py")
+os.system("python Proyecto2-parte1.py")
+os.system("python Proyecto2-parte2.py")
+os.system("python Proyecto2-parte3.py")
+os.system("python Proyecto2-parte4.py")
 import tkinter
 from tkinter import *
 
